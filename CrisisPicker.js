f004eb94410a77712377e21f18924ea859b99b63
Kunj Dedhia <kdedhia@umass.edu>
2019-10-21 23:27:49 -0400

    css fixes

--- CrisisPicker.js
+++ CrisisPicker.js
@@ -9,7 +9,7 @@ const dataVal = data.crisis;
 class CrisisPicker extends Component {
     state = { id: '', crisis: '', contact: '', respond: '', report: '' }
     updateCrisis = (id) => {
-        this.setState({ id: id})
+        this.setState({ id: id })
         if (id > -1) {
             this.setState({ id: id, crisis: dataVal[id].name, contact: dataVal[id].refer, respond: dataVal[id].respond, report: dataVal[id].report })
         }
@@ -17,18 +17,17 @@ class CrisisPicker extends Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: "center" }}>
-                <Text style={styles.text}>Share what you know</Text>
+                <Text style={styles.title}>Share what you know</Text>
                 <Picker
                     selectedValue={this.state.id}
-                    style={{ height: 100, width: width * 8 / 10, marginTop: height / 10 }}
+                    style={styles.dropdown}
                     onValueChange={this.updateCrisis}>
-                    <Picker.Item label="Select one" value="-1"/>
+                    <Picker.Item label="Select one" value="-1" />
                     {dataVal.map((item, index) => {
                         return (<Picker.Item label={item.name} value={index} key={index} />)
                     })}
                 </Picker>
-                <Text style={styles.text}>{this.state.id}</Text>
-                <Text>{this.state.contact}</Text>
+                <Text style={styles.textContact}>{this.state.contact}</Text>
                 <Text>{this.state.respond}</Text>
                 <Text>{this.state.report}</Text>
             </View>
@@ -38,9 +37,20 @@ class CrisisPicker extends Component {
 export default CrisisPicker
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 30,
+    textContact: {
+        fontSize: 20,
+        textAlign: "center",
         fontFamily: 'Cochin',
         marginTop: height / 5
+    },
+    title: {
+        fontSize: 30,
+        fontFamily: 'Cochin',
+        marginTop: height / 10
+    },
+    dropdown: {
+        height: 100,
+        width: width * 8 / 10,
+        marginTop: height / 20
     }
 })
